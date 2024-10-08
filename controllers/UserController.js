66e2ef3f55169bb01ea48dcf6bd3df96a41ee8a9
Charles Obika <Charlesobika@yahoo.com>
2024-10-08 13:11:16 +0200

    Your commit message here

---
+++ controllers/UserController.js
@@ -0,0 +1,15 @@
+const User = require('../models/User');
+
+// Create a new user
+const createUser = async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        const newUser = new User({ username, email, password });
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+module.exports = { createUser };
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
